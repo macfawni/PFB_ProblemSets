542a66f7730490ebf775a1c4daeb55379ae0f0a7
pfb01 <pfb01@info01.cshl.edu>
2023-10-17 15:31:20 -0400

    added less than 50 condition

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 x = 5
-if x > 0:
-  message = "positive"
+if x > 0 and x < 50:
+  message = "positive and less than 50"
   print(message)
 if x < 0:
   message = "negative"
   print(message)
-
+elif x == 0:
+  message = "equals zero!"
+  print(message)
