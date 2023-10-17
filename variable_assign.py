2d288d96114238bb62c1295316910ea38d34b1e1
pfb01 <pfb01@info01.cshl.edu>
2023-10-17 15:07:50 -0400

    variable true false assg.

---
+++ variable_assign.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+x = 500
+if x > 400 
+   message = "value over 400"
+   print(count, message)
+elif x >= 500
+   message = "that is a large number"
+   print(count, message)
+else:
+   print('not true')
