3f057ce2e7158086b1d92631a183a6128b3f2819
pfb01 <pfb01@info01.cshl.edu>
2023-10-17 15:43:30 -0400

    nested a new variable <50

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 x = 5
-if x > 0 and x < 50:
-  message = "positive and less than 50"
+if x > 0:
+  message = "positive"
   print(message)
+  if x < 50:
+    message = "it is less than 50"
+    print(message)
 if x < 0:
   message = "negative"
   print(message)
