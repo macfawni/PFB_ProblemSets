fc4d7c70e0c5649e8cc1f01c562a11b8f593887f
pfb01 <pfb01@info01.cshl.edu>
2023-10-17 16:13:06 -0400

    divisible by 3 update

--- pos_or_neg.py
+++ pos_or_neg.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
-x = 6
+x = 60
 if x > 0:
   message = "positive"
   print(message)
   if x < 50 and x % 2 == 0:
     message = "it is an even number that is smaller than 50"
+    print(message)
+  elif x > 50 and x % 3 == 0:
+    message = "it is larger than 50 and divisible by 3"
     print(message)
 if x < 0:
   message = "negative"
