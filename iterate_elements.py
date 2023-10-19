636817df95c5022598aed7ae672d8fca2245f3cd
pfb01 <pfb01@info01.cshl.edu>
2023-10-18 20:48:45 -0400

    iterated through list and modulated to find even nos.

---
+++ iterate_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+numbers = [101,2,15,22,95,33,2,27,72,15,52]
+for number in numbers:
+  if (number % 2) == 0:  #this loops number in list divided by 2 and returns value >0 it is odd.
+    print(number)
+ # if number > 0:
+   # print(number)
